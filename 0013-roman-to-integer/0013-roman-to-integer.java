51f86220ea6ef8054508fedd81017306d15b3c7c
Yaswanth sai🧑‍💻 <113105409+yaswanthsai257@users.noreply.github.com>
2024-03-02 18:56:17 +0530

    Time: 6 ms (35.09%), Space: 44.5 MB (85.01%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,8 +1,15 @@
 class Solution {
     public int romanToInt(String str) {
            LinkedHashMap<Character, Integer> map = new LinkedHashMap<>();
-        int sum = 0;
-        res(map);
+         map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+        int sum=0;
+     
         for (int i = 0; i < str.length(); i++) {
             if (i + 1 < str.length() && map.get(str.charAt(i)) < map.get(str.charAt(i + 1))) {
                 sum -= map.get(str.charAt(i));
@@ -14,14 +21,5 @@ class Solution {
         return sum;
     }
 
-    private void res(LinkedHashMap<Character, Integer> map) {
-        map.put('I', 1);
-        map.put('V', 5);
-        map.put('X', 10);
-        map.put('L', 50);
-        map.put('C', 100);
-        map.put('D', 500);
-        map.put('M', 1000);
-    }
-        
+ 
     }
