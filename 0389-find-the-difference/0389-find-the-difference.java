76f54f39df30d6d1284e6d89b8daedb5902d0729
Yaswanth sai🧑‍💻 <113105409+yaswanthsai257@users.noreply.github.com>
2024-03-09 18:59:58 +0530

    Time: 2 ms (65.20%), Space: 41.6 MB (78.38%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -0,0 +1,26 @@
+class Solution {
+    public char findTheDifference(String str, String t) {
+     int[] a=new int[26];
+        int[] b=new int[26];
+       int n=t.length();
+        for(int i=0;i<n-1;i++){
+            char ch1=str.charAt(i);
+            char ch2=t.charAt(i);
+            
+            a[ch1-97]++;
+            b[ch2-97]++;
+                
+            
+        }
+        b[t.charAt(n-1)-97]++;
+        
+        for(int i=0;i<26;i++){
+            if(a[i]!=b[i]) return (char)(i+97);
+        }
+        
+        return 'z';
+        
+        
+        
+    }
+}
